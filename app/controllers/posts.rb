791b81abefbc27b02e4ea48e4dc54832df7a4611
Goraya <naeemgoraya@yahoo.com>
2013-09-17 12:28:50 +0500

    initial commit

---
+++ app/controllers/posts.rb
@@ -0,0 +1,57 @@
+AppBlog::App.controllers :posts do
+		
+	get :index, :map =>'/' do
+		@title = "Posts"
+		@posts = Post.all
+		render 'posts/index'
+	end
+	
+	get :new, :map =>'/new' do
+		@post = Post.new
+		render 'posts/new'
+	end
+	
+	post :create  do
+		@post = Post.new(params[:post])
+			if @post.save
+				flash[:notice] = 'Post was successfully created.'
+				redirect url(:posts, :index)
+			else
+		render 'posts/new'
+			end
+	end
+	
+	delete :destroy, :csrf_protection => false do
+		@post = Post.find(params[:id])
+			if@post.destroy
+				flash[:notice] = 'Post was successfully deleted.'
+				redirect url(:posts, :index)
+			else
+		flash[:notice] = 'Post was not deleted.'
+			end
+	end
+	
+	get :edit, :map=>'/edit' do
+		@post = Post.find(params[:post_id])
+			if @post
+				render 'posts/edit'
+			else
+      
+			end
+	end
+	 
+	 put :update do
+    
+    @post = Post.find(params[:id])
+			if @post
+				if @post.update_attributes(params[:post])
+					redirect url(:posts, :index)
+				else
+					render 'posts/edit'
+				end
+			else
+		end
+  end
+
+end
+	
